67e7d560c2c53ea4e78685fed31791e103e39ee7
Kingshuk Banerjee <39654464+kinshukb@users.noreply.github.com>
2024-03-31 11:54:23 +0530

    Update loadbalancer.go

--- loadbalancer.go
+++ loadbalancer.go
@@ -4,43 +4,43 @@ import "log"
 
 
 // A weighted approach to balance loads
-type ListenerPod struct {
+type Listener struct {
 	NCname string `json:"ncname"`
-	PodId  string `json:"podid"`
+	Id     string `json:"id"`
 	Weight int    `json:"weight"`
 }
 
 
-//List of Weighted ListenerPods for load control.
-var ListenerPods []ListenerPod
+//List of Weighted Listener for load control.
+var Listeners []Listener
 
 
-// Imp: We need both the cached one and ListenerPods balancing, pod will be selected from below array, cache will help in the process.
+// Imp: We need both the cached one and Listeners balancing, will be selected from below array, cache will help in the process.
 func selectListener(ncname string) string {
-	var finalPodId string
-	for i := 0; i < len(ListenerPods); i++ {
-		if ListenerPods[i].Weight == 0 && ListenerPods[i].NCname == ncname {
-			ListenerPods[i].Weight = ListenerPods[i].Weight + 1
-			finalPodId = ListenerPods[i].PodId
-			return finalPodId
+	var final string
+	for i := 0; i < len(Listeners); i++ {
+		if Listeners[i].Weight == 0 && Listeners[i].NCname == ncname {
+			Listeners[i].Weight = Listeners[i].Weight + 1
+			final = Listeners[i].Id
+			return final
 		}
-		for j := i + 1; j < len(ListenerPods); j++ {
-			if ListenerPods[j].Weight < ListenerPods[i].Weight && ListenerPods[j].NCname == ncname {
+		for j := i + 1; j < len(Listeners); j++ {
+			if Listeners[j].Weight < Listeners[i].Weight && Listeners[j].NCname == ncname {
 				log.Println("-----------balancing---------", i))
-				ListenerPods[j].Weight = ListenerPods[j].Weight + 1
-				finalPodId = ListenerPods[j].PodId
-				return finalPodId
+				Listeners[j].Weight = Listeners[j].Weight + 1
+				final = Listeners[j].Id
+				return final
 			}
 		}
-		if ListenerPods[i].NCname == ncname {
+		if Listeners[i].NCname == ncname {
 			logger.Debug("already balanced :", zap.Int("i: ", i))
-			ListenerPods[i].Weight = ListenerPods[i].Weight + 1
-			finalPodId = ListenerPods[i].PodId
-			return finalPodId
+			Listeners[i].Weight = Listeners[i].Weight + 1
+			final = Listeners[i].Id
+			return final
 		}
 
 	}
-	return finalPodId
+	return final
 }
 
 
